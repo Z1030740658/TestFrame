75f46ad7cbbdce2147fc820f35b38d247d25c61e
Admin <Admin@LAPTOP-TOPCHANI>
2020-08-27 12:59:11 +0300

    pytest warning solved

--- utils/enums/urls.py
+++ utils/enums/urls.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class TestUrls(Enum):
+class Urls(Enum):
     """
     All kind of web urls for test to concatenate with base url
     """
--- web_pages/google_page.py
+++ web_pages/google_page.py
@@ -1,6 +1,6 @@
 from web_pages.base_page import BasePage
 from configs import WEB_URL
-from utils.enums.test_urls import TestUrls
+from utils.enums.urls import Urls
 from web_pages.search_result_page import SearchResultPage
 from selenium.webdriver.common.keys import Keys
 
@@ -10,7 +10,7 @@ class GooglePage(BasePage):
         super().__init__(driver)
 
     def search(self, question):
-        self.driver.get(WEB_URL + TestUrls.test_url.value)
+        self.driver.get(WEB_URL + Urls.test_url.value)
         search_input = self.wait.until_element_visible_by_name('q')
         search_input.send_keys(question)
         search_input.send_keys(Keys.ENTER)
