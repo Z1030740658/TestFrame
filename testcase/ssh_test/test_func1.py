fce8b799fd9e8fb32b03b89287307bec3dd820ff
zjl <1030740658@qq.com>
2023-05-25 03:20:37 +0000

    修改測試用例

--- testcase/ssh_test/test_func1.py
+++ testcase/ssh_test/test_func1.py
@@ -1,14 +1,14 @@
-from services.ssh import BaseSSHClient
+from services.ssh.ssh_client import BaseSSHClient
 
 class TestDemon:
     def setup(self):
         self.ssh_client = BaseSSHClient()
 
-def test_func1(self):
-    cmd = 'uname -a'
-    self.ssh_client.execute(cmd)
+    def test_func1(self):
+        cmd = 'uname -a'
+        self.ssh_client.execute_one(cmd)
 
-def teardown(self):
-    self.basepage.driver.quit()
+    def teardown(self):
+        pass
 # pipenv run pytest tests/web_ui
 # pipenv run pytest tests/web_ui/test_search_for_driver_docs.py
