45c7efab522790f0bb618573126fd1919e84ae1f
zjl <1030740658@qq.com>
2023-05-30 11:31:54 +0800

    新增selenium公共方法

--- web_pages/base_page.py
+++ web_pages/base_page.py
@@ -3,6 +3,7 @@ from web_pages.waits import Waits
 from utils.logger import log
 from selenium.webdriver.common.by import By
 from fixtures.webdriver_setup import get_browser_driver
+from selenium.webdriver.common.keys import Keys
 
 
 class BasePage:
@@ -29,6 +30,42 @@ class BasePage:
                 log.info("PAGE_LOADED")
                 break
 
-    def find_by_xpath(self, xpath):
+    def find_ele_xpath(self, value):
         """find element by xpath"""
-        self.driver.find_element(by=By.XPATH, value=xpath)
+        return self.driver.find_element(by=By.XPATH, value=value)
+
+    def find_ele_id(self, value):
+        """find element by id"""
+        return self.driver.find_element(by=By.ID, value=value)
+
+    def find_ele_name(self, value):
+        """find element by name"""
+        return self.driver.find_element(by=By.NAME, value=value)
+
+    def find_ele_classname(self, value):
+        """find element by classname"""
+        return self.driver.find_element(by=By.CLASS_NAME, value=value)
+
+    def find_ele_css(self, value):
+        """find element by css"""
+        return self.driver.find_element(by=By.CSS_SELECTOR, value=value)
+
+    def find_eles_xpath(self, value):
+        """find elements by xpath"""
+        return self.driver.find_element(by=By.XPATH, value=value)
+
+    def find_eles_id(self, value):
+        """find elements by id"""
+        return self.driver.find_element(by=By.ID, value=value)
+
+    def find_eles_name(self, value):
+        """find elements by name"""
+        return self.driver.find_element(by=By.NAME, value=value)
+
+    def find_eles_classname(self, value):
+        """find elements by classname"""
+        return self.driver.find_element(by=By.CLASS_NAME, value=value)
+
+    def find_eles_css(self, value):
+        """find elements by css"""
+        return self.driver.find_element(by=By.CSS_SELECTOR, value=value)
